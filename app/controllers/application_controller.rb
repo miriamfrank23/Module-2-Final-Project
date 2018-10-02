a2466d9b2439ac48a8d83d8c025fa39c345163c6
sinyoungnam <sinnam90@gmail.com>
2018-10-02 16:45:30 -0400

    sin updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  helper_method :current_user
+
   def require_login
     return head(:forbidden) unless session.include? :user_id
+  end
+
+  def current_user
+    User.find_by({ id: session[:user_id] })
   end
 
 end
