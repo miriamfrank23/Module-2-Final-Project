5a15cac9c6caf3928f09a61d2d1f6caef0deb871
github username <github email address>
2018-10-03 19:35:03 -0400

    integrate bookings and reviews to master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_200332) do
+ActiveRecord::Schema.define(version: 2018_10_02_172615) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id"
@@ -39,12 +39,12 @@ ActiveRecord::Schema.define(version: 2018_10_03_200332) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.float "rating"
+    t.integer "rating"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
